c154b07ccdc8fabbc6814c51d62d6bc67225a00b
Paulo Pizarro <paulo.pizarro@gmail.com>
2013-11-20 19:01:15 -0200

    Using restify template application

--- tests/delete_account.py
+++
@@ -1,5 +0,0 @@
-from urllib2 import Request, urlopen
-request = Request("http://emailserviceinterface.apiary.io/accounts/{id}")
-request.get_method = lambda: 'DELETE'
-response_body = urlopen(request).read()
-print response_body
--- tests/get_account.py
+++
@@ -1,4 +0,0 @@
-from urllib2 import Request, urlopen
-request = Request("http://emailserviceinterface.apiary.io/accounts/{id}")
-response_body = urlopen(request).read()
-print response_body
